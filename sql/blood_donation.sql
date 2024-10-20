c66ebef5f7361e2af50ccf5babae7c409a9846d4
Mr.CodeCrafter <138376815+MrCodeCrafter@users.noreply.github.com>
2024-10-20 23:25:37 +0530

    Update blood_donation.sql

--- sql/blood_donation.sql
+++ sql/blood_donation.sql
@@ -57,6 +57,22 @@ CREATE TABLE `donations` (
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `hospitalstaffs`
+--
+
+DROP TABLE IF EXISTS `hospitalstaffs`;
+CREATE TABLE `hospitalstaffs` (
+  `id` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `hospital_id` int(10) NOT NULL,
+  `email` varchar(100) NOT NULL,
+  `password` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `hospitals`
 --
 
